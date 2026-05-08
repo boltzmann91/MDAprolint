d9fd2f469e349d74968da85d7636dc547a24d429
Sanket Walujkar <swalujkar@fas.harvard.edu>
2026-05-08 13:29:47 -0400

    Fixed a minor issue with __init__.py.

--- src/MDAprolint/__init__.py
+++ src/MDAprolint/__init__.py
@@ -1,11 +1,11 @@
 # Import the functions from their specific modules
-from .interactions import get_interacting_lipids_openmp
+from .interactions import get_interacting_lipids
 from .kinetics import calculate_residence_times
 from .distances import get_lipid_protein_distances
 
 # Expose them to the user
 __all__ = [
-    "get_interacting_lipids_openmp",
+    "get_interacting_lipids"
     "calculate_residence_times",
     "get_lipid_protein_distances"
 ]
